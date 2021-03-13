9e03be9b4713739d87467337f77743549ca2839c
Kris Litman <kris.d.litman@gmail.com>
2021-03-13 18:47:11 -0500

    Webmock ready

--- spec/features/api/consume_giphy_api_spec.rb
+++ spec/features/api/consume_giphy_api_spec.rb
@@ -1,7 +1,11 @@
 require "rails_helper"
 
 RSpec.describe "Giphy API" do
-  it "Can be consumed" do
-    # api.giphy.com/v1/gifs/random?api_key=QohTU31I9NDv6lAAsU5wWOjreciGjMwQ&tag=dog&rating=g
+  scenario "Api can be consumed" do
+    stub_request(:get, 
+    "api.giphy.com/v1/gifs/random?api_key=QohTU31I9NDv6lAAsU5wWOjreciGjMwQ&tag=dog&rating=g").
+      to_return(status: 200, body: "")
+
+    visit '/'
   end
 end
