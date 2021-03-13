269a353e10728a7b3f7fae1956d1cafeba81a71e
Kris Litman <kris.d.litman@gmail.com>
2021-03-13 18:52:29 -0500

    Added schema, and json fixture for random dog giphy

--- spec/features/api/consume_giphy_api_spec.rb
+++ spec/features/api/consume_giphy_api_spec.rb
@@ -2,9 +2,10 @@ require "rails_helper"
 
 RSpec.describe "Giphy API" do
   scenario "Api can be consumed" do
+    json_response = File.read('spec/fixtures/random_dog_giphy.json')
     stub_request(:get, 
     "api.giphy.com/v1/gifs/random?api_key=QohTU31I9NDv6lAAsU5wWOjreciGjMwQ&tag=dog&rating=g").
-      to_return(status: 200, body: "")
+      to_return(status: 200, body: json_response)
 
     visit '/'
   end
