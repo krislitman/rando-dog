637bb70c135f5112836d6d98bed894687f9110e9
Kris Litman <kris.d.litman@gmail.com>
2021-03-13 18:36:24 -0500

    building out test for api consumption

---
+++ spec/features/api/consume_giphy_api_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+RSpec.describe "Giphy API" do
+  it "Can be consumed" do
+    # api.giphy.com/v1/gifs/random?api_key=QohTU31I9NDv6lAAsU5wWOjreciGjMwQ&tag=dog&rating=g
+  end
+end
