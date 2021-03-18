19a28a5b5ee90fbf5fb001ce05c16241712f4bfb
Kris Litman <kris.d.litman@gmail.com>
2021-03-18 14:29:48 -0400

    Adding tests for api consumption

--- spec/features/api/consume_giphy_api_spec.rb
+++ spec/features/api/consume_giphy_api_spec.rb
@@ -2,11 +2,17 @@ require "rails_helper"
 
 RSpec.describe "Giphy API" do
   scenario "Api can be consumed" do
+
     json_response = File.read('spec/fixtures/random_dog_giphy.json')
     stub_request(:get, 
-    "api.giphy.com/v1/gifs/random?api_key=QohTU31I9NDv6lAAsU5wWOjreciGjMwQ&tag=dog&rating=g").
+    "api.giphy.com/v1/gifs/random?api_key=#{Figaro.env.api_key}&tag=dog&rating=g").
       to_return(status: 200, body: json_response)
 
-    visit '/'
+      body = JSON.parse(json_response)
+
+      doggo = Rando.new(body)
+
+      expect(doggo.type),to eq('gif')
+
   end
 end
