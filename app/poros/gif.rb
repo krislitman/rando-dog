395a4f1139abc80bdfdf669013a3e8e7886beacb
Kris Litman <kris.d.litman@gmail.com>
2021-03-20 15:26:57 -0400

    Images are working

--- app/poros/gif.rb
+++ app/poros/gif.rb
@@ -2,12 +2,14 @@ class Gif
   attr_reader :type,
               :url,
               :title,
-              :images
+              :images,
+              :image_url
   
   def initialize(input)
     @type = input["data"]["type"]
     @url = input["data"]["url"]
     @title = input["data"]["title"]
     @images = input["data"]["images"]
+    @image_url = input["data"]["image_url"]
   end
 end
