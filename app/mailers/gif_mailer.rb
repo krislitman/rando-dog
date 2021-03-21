196fa698985062ce6c984316fb38cb9c4def61a4
Kris Litman <kris.d.litman@gmail.com>
2021-03-21 15:09:57 -0400

    Can send email in development

--- app/mailers/gif_mailer.rb
+++ app/mailers/gif_mailer.rb
@@ -1,9 +1,10 @@
 class GifMailer < ApplicationMailer
 
-  def new_email(gif)
-    @greeting = "#{gif.title}"
+  def new_email(gif, email_address)
+    @greeting = "Here is your Random Dog Gif!"
+    @gif = gif
 
-    mail to: "#{params[:sender]}",
+    mail to: "#{email_address}",
          subject: "New Gif just for you!"
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,13 +62,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "kris.d.litman@gmail.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: Figaro.env.gmail_username,
-    password: Figaro.env.gmail_password
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            Figaro.env.gmail_username,
+    password:             Figaro.env.gmail_password,
+    authentication:       :plain,
+    enable_starttls_auto: true
   }
 
   config.action_mailer.default_url_options = { host: "localhost:3000" }
